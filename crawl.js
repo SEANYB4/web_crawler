4fb74af89b1d557c1d817c42587b216fdb61aa39
Sean Bain <artemis45566@gmail.com>
2023-07-03 18:08:09 +0800

    crawlPage recursive

--- crawl.js
+++ crawl.js
@@ -4,7 +4,22 @@ const { link } = require('fs');
 
 
 
-const crawlPage = async (currentURL) => {
+const crawlPage = async (baseURL, currentURL, pages) => {
+
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;
 
     console.log(`actively crawling: ${currentURL}`);
 
@@ -14,19 +29,30 @@ const crawlPage = async (currentURL) => {
         
         if (resp.status > 399) {
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type");
         if (!contentType.includes("text/html")) {
             console.log(`non html response, content type: ${contentType}, on page ${currentURL}`);
-            return
+            return pages
         }
 
-        console.log(await resp.text());
+        const htmlBody = await resp.text();
+        nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for (const nextUrl of nextURLs) {
+            pages = await crawlPage(baseURL, nextUrl, pages);
+        }
+
+        
+
+
     } catch (error) {
         console.log(`error in fetch: ${error.message} on page ${currentURL}`);
     }
+
+    return pages;
     
     
 }
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { normalizeURL, getURLsFromHTML, crawlPage } = require('./crawl.js');
 
 
-function main() {
+async function main() {
 
     if (process.argv.length < 3) {
         console.log('No website provided');
@@ -20,7 +20,13 @@ function main() {
     const baseURL = process.argv[2]
     console.log(`starting web crawl of ${baseURL}`);
 
-    crawlPage(baseURL);
+    // crawl pages returns a promise so we need to wait until that resolves
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+    // make an object iterable
+    for (const page of Object.entries(pages)) {
+        console.log(page);
+    }
 }
 
 
